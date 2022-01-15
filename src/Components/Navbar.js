3d93f1be4a395d38e60fb26375088b086e016f4b
Neha <codewithneha2020@gmail.com>
2022-01-15 16:40:25 +0530

    Navbar

---
+++ src/Components/Navbar.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+export default function Navbar() {
+    const myStyle = {
+         color: 'red',
+         fontFamily:'Times New Roman, Times, serif',
+         fontSize: '18pt',
+         
+    }
+    return(
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <div className="container-fluid">
+    <a className="navbar-brand" style={myStyle} href="#"><b>zomato</b></a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link" aria-current="page" href="#">Products</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Restaurants</a>
+        </li>
+      
+        <li className="nav-item">
+          <a className="nav-link " href="#" tabindex="-1">Pricing</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#" tabindex="-1">About us</a>
+        </li>
+      </ul>
+      <form className="d-flex">
+        <button className="btn btn-danger" type="submit">Order Food</button>
+      </form>
+    </div>
+  </div>
+</nav>
+    )
+}
